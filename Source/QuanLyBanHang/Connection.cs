d461974edbac9e602770b161ff2b04840dd58085
Tung110320 <phanthanhtung110320@gmail.com>
2023-03-03 16:57:56 +0700

    Add folder Source

---
+++ Source/QuanLyBanHang/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHang
+{
+    class Connection
+    {
+        public static SqlConnection connect = new SqlConnection(@"Data Source=LAPTOP-BPREQTBQ\SQLEXPRESS;Initial Catalog=QLBanHang;Integrated Security=True");
+    }
+}
